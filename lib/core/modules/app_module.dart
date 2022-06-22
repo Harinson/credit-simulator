259213fa159e8dc6b2d5869e0ce22dd45086d417
Igor Harinson Mendes Monteiro <igorharinson.mm@gmail.com>
2022-06-22 15:56:48 -0300

    feat: implement flutter_modular

---
+++ lib/core/modules/app_module.dart
@@ -0,0 +1,16 @@
+import 'package:credit_simulator/presenter/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          '/',
+          child: (ctx, args) => const HomePage(),
+        ),
+      ];
+}
